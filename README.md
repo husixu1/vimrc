08a6e1d83c59da9d94225a511199e6c6ac436185
husixu1 <husixu1@hotmail.com>
2017-03-01 15:30:14 +0800

    clewn added, one-key-install script added

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# TO USE THIS FILE
+# TO USE THIS CONFIG
 
-* put all directories into your ~/.vim
-* put vimrc in your home dir (a.k.a. ~/)
+## auto-installation
+---
+run `./one-key-install.sh` WITHOUT root privilige
+
+## manual-installation
+---
+
+* put all directories into your .vim dir 
+* put vimrc in your home dir
 * rename it to .vimrc
 * run `curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
 * run `vim +PlugInstall`
 
 ### YCM config
-
----
 
 either-> create your own .ycm\_extra\_conf.py and put it in ~/.vim/custom
 or-----> use the ycm\_extra\_conf.py in this repo insted (do not have to change anything)
---
+++ one-key-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# check necessary commands
+command -v vim > /dev/null 2>&1 || echo >&2 "vim not exist"
+command -v python > /dev/null 2>&1 || echo >&2 "python not exist"
+command -v curl > /dev/null 2>&1 || echo >&2 "curl not found"
+
+echo "your \$HOME dir is $HOME"
+[ -d "$HOME/.vim" ] || echo "$HOME/.vim not exist, creating one ... "; mkdir -p "$HOME/.vim"
+[ -d "$HOME/.vim" ] || echo "$HOME/.vim/autoload not exist, creating one ..."; mkdir -p "$HOME/.vim/autoload"
+
+cp ./vimrc "$HOME/.vimrc"
+cp -r ./custom "$HOME/.vim/custom"
+
+# install vim-plug
+[ -f "$HOME/.vim/autoload/plug.vim" ] || curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[ -f "$HOME/.vim/autoload/plug.vim" ] || echo "vim-plug download failed, please change the url or download manually ... "; exit
+
+vim +PlugInstall
+
+# install clewn
+cd ./clewn-1.15 || echo "clewn-1.15 not exist"; exit
+export vimdir="$HOME/.vim"
+./configure --prefix="HOME/.vim"
+./make && make install
+
